9b2d5bbf53007f3b0fd8122986a4edd530549840
Mark Oosterveld <mark@oosterveld.org>
2016-07-17 15:43:18 -0400

    fix scope of servie worker

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 (function() {
   if (navigator.serviceWorker) {
-    navigator.serviceWorker.register('/src/sw.js', {scope: '/'}).then(function(registration) {
+    navigator.serviceWorker.register('/sw.js').then(function(registration) {
       console.log('ServiceWorker registration successful with scope:',  registration.scope);
     }).catch(function(error) {
       console.log('ServiceWorker registration failed:', error);
