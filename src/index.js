80d394888c1fa5c6b2f589589e720972b5227156
Mark Oosterveld <mark@oosterveld.org>
2016-07-17 15:40:26 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 (function() {
   if (navigator.serviceWorker) {
-    navigator.serviceWorker.register('/src/sw.js').then(function(registration) {
+    navigator.serviceWorker.register('/src/sw.js', {scope: '/'}).then(function(registration) {
       console.log('ServiceWorker registration successful with scope:',  registration.scope);
     }).catch(function(error) {
       console.log('ServiceWorker registration failed:', errror);
